2e93a4a12e1024c6aea92dc86901223982650614
Jiawei Lu <jl5999@columbia.edu>
2022-02-13 23:36:53 -0500

    Initial commit

---
+++ RetinaNet/detect_utils.py
@@ -0,0 +1,14 @@
+import torchvision.transforms as transforms
+import cv2
+import numpy as np
+import torch
+
+from coco_names import COCO_INSTANCE_CATEGORY_NAMES as coco_names
+
+# this will help us create a different color for each class
+COLORS = np.random.uniform(0, 255, size=(len(coco_names), 3))
+
+# define the torchvision image transforms
+transform = transforms.Compose([
+    transforms.ToTensor(),
+])
